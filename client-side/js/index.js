5f686f8b6b0c5c1184213b97385908cc1f7be252
Muhammad <razaqfatiu@gmail.com>
2019-05-31 09:35:06 +0100

    create search one data

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,38 +1,44 @@
 $(function() {
+$("#one").hide()
     $.ajax({
         method: 'GET',
         url: 'http://localhost:3000/animals',
         dataType: 'json',
     }).done(function(data){
-        // console.log(data);
         $.map(data, function(post, i){
             $("#structured").append(`
-            <div class="card border border-primary rounded each" style="width: 18rem;" id=${post.id}>
+            <div class="card border border-primary rounded text-center each" style="width: 18rem;" id=${post.id}>
             <img class="card-img-top" src=${post.image} alt="Card image cap" height="200">
             <div class="card-body">
               <h5 class="card-title">${post.scientificName}</h5>
+              <p class="card-text">A ${post.sex} ${post.name}.</p>
               <p class="card-text">Groups: ${post.group}.</p>
-              <button class="btn btn-primary" id="detail">See Details</button>
+              <p class="card-text">Among the wildest: ${post.species}.</p>
             </div>
           </div>`)
         })
     })
 
-  $("#jumb").hide()
-  $("#search").click(function(){
-    const id = $("#text").val()
+  $("#search").click(function(e){
+    e.preventDefault()
+    const name = $("#text").val()
+
     $.ajax({
       method: 'GET',
-      url: 'http://localhost:3000/animals/'+id,
+      url: `http://localhost:3000/animals/?name=${name}`,
       dataType: 'json',
   }).done(function(data){
+    $.map(data, function(post, i){
       console.log(data);
-      $(window).load(function() {
-
-        jQuery.noConflict();
-            $('#myModal').modal('show');
-        });
+      const {image,name,scientificName,group,sex} = post
+      $("#img").attr("src", image)
+      $("#atittle").text(name)
+      $("#sciname").text(scientificName)
+      $("#group").text(group)
+      $("#one").show()
+    })
   })
     
-  })
+  })   
+
   })
